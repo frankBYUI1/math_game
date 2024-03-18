d0dcd2082c1406999ee62d1332620de4374596b4
sunnyyy7 <yeu20001@byui.edu>
2024-03-18 13:19:54 -0600

    Added check answer function

---
+++ checkanswer.js
@@ -0,0 +1,11 @@
+function checkAnswer(num1, num2, answer, correctAnswer, incorrectAnswer) {
+    if (num1 + num2 ===answer){
+        console.log(correctAnswer);
+    } else {
+        comnsole.log(incorrectAnsewr);
+    }
+}
+
+// Sample ideas :
+checkAnswer(3, 4, 7, "Congratulations! Your answer is correct.");
+checkAnswer(2, 5, 10, "Congratulations! Your answer is correct.");
