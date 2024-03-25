b37ce82787a173d551e4c58763964a740ac72f6d
Ethan Houser <hou21013@byui.edu>
2024-03-25 13:00:59 -0600

    Updated index.js to have the checkanswers function, as well as basic style changes in the coding.

--- checkanswer.js
+++
@@ -1,7 +0,0 @@
-function checkAnswer(num1, num2, answer) {
-    if (num1 + num2 === answer) {
-        console.log("Correct!");
-    } else {
-        console.log("Incorrect");
-    }
-}
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,45 @@
-const gameBox = document.getElementById("gameBox");
-const mathBox = document.getElementById("mathBox");
+const gameBox    =  document.getElementById("gameBox");
+const mathBox    =  document.getElementById("mathBox");
+const userAnswer =  document.getElementById("answer");
+const question   =  document.getElementById("question");
 
-function createGameBox() {
+
+// EQUATION GENERATION FUNCTIONS
+
+function genAddition(difficulty)
+{
+    // Generates a randomized addition equation using only whole positive numbers. 
+    // Returns a tuple of 3 integers being num1, num2, and  answer
+
+    // Code goes here...
+}
+function genSubtraction(difficulty)
+{
+    // Generates a randomized subtraction equation using only whole positive numbers. 
+    // Returns a tuple of 3 integers being num1, num2, and  answer
+
+    // Code goes here...
+}
+function genMultiplication(difficulty)
+{
+    // Generates a randomized multiplication equation using only whole positive numbers. 
+    // Returns a tuple of 3 integers being num1, num2, and  answer
+
+    // Code goes here...
+}
+function genDivision(difficulty)
+{
+    // Generates a randomized division equation using only whole positive numbers. 
+    // Returns a tuple of 3 integers being num1, num2, and  answer
+
+    // Code goes here...
+}
+
+
+// UPDATE HTML FUNCTIONS
+
+function createGameBox() 
+{
     const testElement = document.createElement("div");
     testElement.classList.add("gameBox");
     testElement.innerHTML =
@@ -19,8 +57,8 @@ function createGameBox() {
         ;
     testElement.appendChild(playArea);
 }
-
-function createEquationBox() {
+function createEquationBox() 
+{
     const equationBox = document.createElement("div");
     equationBox.classList.add("mathBox");
     equationBox.innerHTML = 
@@ -43,6 +81,34 @@ function createEquationBox() {
     ;
     equationBox.appendChild(numberBox);
 }
+function checkAnswer(num1, num2, userAnswer) 
+{
+    // Checks the user's input and compares it to the question to 
+    // see if the user's answer matches the equations correct answer.
+
+    if (num1 + num2 === userAnswer) {
+
+        // Output to html page Correct
+
+        // console.log("Correct!");
+    } else {
+
+        // Output to html page Incorrect
+
+        // console.log("Incorrect");
+    }
+}
+function updateWebEquation(num1, num2, answer)
+{
+    // Updates the HTML to show a new equation provided
+    // from the passed parameters.
+
+    // Code goes here...
+}
+
+
+
+
 
 createGameBox();
 createEquationBox();
